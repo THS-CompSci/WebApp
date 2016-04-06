acb4fc68183d6fa8cce11c2fc33a36b07bd7b74f
Damen500 <soccermandamen@gmail.com>
2016-04-06 14:19:57 -0500

    Create DatabaseConnection.php

---
+++ api/DatabaseConnection.php
@@ -0,0 +1,14 @@
+<?php
+$connection =null;
+    function DBConnect($serverName,$serverUsername,$serverPassword,$dbName){
+        global $connection;
+        $connection=mysqli_connect($serverName,$serverUsername,$serverPassword,$dbName);
+    }
+    function createQuery($databaseQuery){
+        global $connection;
+        return mysqli_query($connection,$databaseQuery);
+    }
+    function getConnection(){
+        global $connection;
+        return $connection;
+    }
