9f6be0afc3b111197f8574a761ced1150289329f
Santiago <sntgcortes23@gmail.com>
2016-04-09 13:43:06 -0500

    Finished tScript.js
    I removed some unnecessary code and fully implemented the server request-to-table functionality. The code for the pass history is now complete, but keep in mind that this script is for passHistory.html only so functions like parseURLParams() should be moved elsewhere.

--- tScript.js
+++ tScript.js
@@ -1,13 +1,7 @@
-/**
- * Fills a given table with rows
- *
- * ind - the index of the table
- * tables - the number of rows to fill the table with
- * fillText - the text to fill each row with
- */
 //Could be used for generation of pages through QR codes. With this, the html can take variables from the URL and turn them into arrays.
 //For example: parseURLParams("www.thshallpass.com?name=something") returns {name: ["something"]}
- 
+//-------This function should be moved to a separate file relating to the qr generation, but realistically doesn't need to exist
+//-------as php can already read POST data from urls.
  function parseURLParams(url) {
     var queryStart = url.indexOf("?") + 1,
         queryEnd   = url.indexOf("#") + 1 || url.length + 1,
@@ -32,58 +26,11 @@
     }
     return parms;
 }
- 
- 
- 
-function getTable(ind, tables, fillText){
-    //Gets the HTML of the table with the given class 'ind'
-    var tableString=jQuery(ind).html();
-    var row=0;
-    for(i=0;i<tableString.length-4;i++){
-        if(tableString.substring(i).indexOf("<tr>")===0){
-            row++;
-        }
-    }
-    //Gets the index of the end of the table and splits the html into two parts, the generated rows go in between both strings
-    var index=tableString.lastIndexOf("</tr>");
-    var strBeg=tableString.substring(0,index+5);
-    var strEnd=tableString.substring(index+5);
-    //Fills up the rows
-    var month=Math.floor(Math.random() * 12) + 1;
-    var day=Math.floor(Math.random() * 28) + 1;
-    var date;
-    if(month>9){
-        date=month;
-    }else{
-        date="0"+month;
-    }
-    if(day>9){
-        date+="/"+day;
-    }else{
-        date+="/0"+day;
-    }
-    var hour=Math.floor(Math.random()*12)+1;
-    var minuteT=Math.floor(Math.random()*7);
-    var minuteO=Math.floor(Math.random()*10);
-    for(i=0;i<tables;i++){
-        strBeg+="<tr><td>"+row+"</td><td>";
-        strBeg+=date+"/16"+"</td><td>"+fillText+"</td><td>";
-        strBeg+=hour+":"+minuteT+minuteO+"</td></tr>";
-        row++;
-    }
-    return strBeg+strEnd;
-}
-
-function addRow(date, name, time){
-    var ret=getTable("#tb1",1,"new time");
-    jQuery("#tb1").html(ret);
-}
 
 function requestStudent(){
     var student=jQuery("#stuID").val();
     jQuery("#tit").text(student);
-    //callPage('passHistory.php?student_id='+student,document.getElementById("testDisplay"));
-    //The php has TEACHERID as the name
+    //This line assumes that passHistory.php requires a 'student_name' parameter
     callPage('passHistory.php?student_name='+student,document.getElementById("testDisplay"));
 }
 
@@ -114,12 +61,22 @@ function callPage(url, div){
         //Request is finished and the response is ready
         if(ajax.readyState==4){
             if(ajax.status==200){
-                //Sets the HTML of the given div to the response text
-                div.innerHTML = ajax.responseText;
+                //Gets the response from the server and then updates the table based on it
+				            var json=ajax.resposeText;
+				            updateTable(jsonString);
             }
         }
     }
     ajax.send(null);
+}
+
+function updateTable(jsonString){
+	    try{
+		    var json=jQuery.parseJSON(jsonString);
+		    $("#table").bootstrapTable('load',json);
+	    }catch(err){
+		    document.getElementById("testDisplay").innerHTML=err;
+	}
 }
 
 jQuery(document).ready(function(){
